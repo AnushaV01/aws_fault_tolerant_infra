e957bf6865e6a73eb112223be413e0d742199c85
AnushaV01 <anusha.vendra0126@gmail.com>
2025-10-27 16:09:58 -0400

    made some more formatting changes

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,18 @@
-output "vpc_id" { value = module.vpc.vpc_id }
-output "public_subnets" { value = module.vpc.public_subnets }
-output "private_subnets" { value = module.vpc.private_subnets }
-output "alb_dns_name" { value = module.compute.alb_dns_name }
-output "rds_endpoint" { value = module.database.rds_endpoint }
+output "vpc_id" { 
+    value = module.vpc.vpc_id
+}
+
+output "public_subnets" { 
+    value = module.vpc.public_subnets
+}
+
+output "private_subnets" { 
+    value = module.vpc.private_subnets
+}
+output "alb_dns_name" { 
+    value = module.compute.alb_dns_name
+}
+
+output "rds_endpoint" { 
+    value = module.database.rds_endpoint 
+}
